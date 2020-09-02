ab2d45de4b535878ba44e98a67f5d2a29bfece1c
Lucas Glenat <lucas.glenat@samsara.com>
2020-09-02 08:35:51 -0700

    try smaller reset low pulse

--- drivers/lora/lr1110.c
+++ drivers/lora/lr1110.c
@@ -214,7 +214,7 @@ void lr1110_hal_reset(const void *context)
 	LOG_DBG("Resetting radio");
 	// high-low-high pulse (declared ACTIVE_LOW in device tree)
 	gpio_pin_set(dev_data.reset, GPIO_RESET_PIN, 1);
-	k_sleep(K_MSEC(1));
+	k_sleep(K_USEC(200));
 	gpio_pin_set(dev_data.reset, GPIO_RESET_PIN, 0);
 	k_sleep(K_MSEC(1));
 }
